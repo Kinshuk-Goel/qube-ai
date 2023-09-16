b70abbb696392416b78112662562a87abb90610b
atharvam2007 <141259883+atharvam2007@users.noreply.github.com>
2023-09-15 22:52:38 -0400

    updating links

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import styles from './index.module.scss';
+import Navbar from '../comps/Navbar';
+import dynamic from 'next/dynamic';
+
 const AboutPage = () => {
 
     return(
