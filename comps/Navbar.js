dcec0b0d367ae90e0ffc608093096c091d9704d1
atharvam2007 <141259883+atharvam2007@users.noreply.github.com>
2023-09-15 23:01:29 -0400

    Update Navbar.js

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -12,9 +12,9 @@ const Navbar = () => {
 
             <div className={styles['navbar-menu']}>
 
-                <Link href="../pages/about.js">
+                <a href="../pages/about.js">
                     <div className={styles['navbar-item']}>About</div>
-                </Link>
+                </a>
 
                 <Link href="/contact">
                     <div className={styles['navbar-item']}>Contact</div>
