3ed74f8466b3ac28c91030ac3e9cc06de9047622
atharvam2007 <141259883+atharvam2007@users.noreply.github.com>
2023-09-16 23:16:50 -0400

    Update SpinningCube.js

--- comps/SpinningCube.js
+++ comps/SpinningCube.js
@@ -35,7 +35,30 @@ function Box(props) {
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01, ref.current.rotation.y += 0.01))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshMatcapMaterial color={hovered ? 'blue' : 'hotpink'} />
+    </mesh>
+  )
+}
+
+function Box2(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x -= 0.01, ref.current.rotation.y -= 0.01))
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
@@ -58,7 +81,7 @@ export default function App() {
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box2 position={[1.2, 0, 0]} />
       <OrbitControls />
     </Canvas>
   )
